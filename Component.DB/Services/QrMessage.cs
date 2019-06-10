1b692773ac0e5778ccf8346e3a06378d49695440
Dariusz <dariuszjarosz@me.com>
2019-06-10 16:18:23 -0500

    Add a standard QrMessage object that can be used whenever a cdb qr code using any method

---
+++ Component.DB/Services/QrMessage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace Component.DB.Services
+{
+    public class QrMessage
+    {
+
+        public static String MESSAGE_SCANNED_TOPIC = "Barcode-Scan";
+
+        public static String QRCODE_CODETYPE = "QRCODE";
+        public static String QRCODE_CODETYPE2 = "QR_CODE";
+
+        public static String QRCODE_START_URL = "qrId=";
+
+        public string CodeType { get; }
+        public string CodeContents { get; }
+
+        public QrMessage(String CodeType, String CodeContents)
+        {
+            this.CodeType = CodeType;
+            this.CodeContents = CodeContents;
+        }
+
+        public int ParseQrCode()
+        {
+            String numString = null;
+            if (CodeType.Equals(QRCODE_CODETYPE) || CodeType.Equals(QRCODE_CODETYPE2))
+            {
+                if (CodeContents.Contains(QRCODE_START_URL))
+                {
+                    int qrIdStartIdx = CodeContents.IndexOf(QRCODE_START_URL);
+
+                    if (qrIdStartIdx > 0)
+                    {
+                        qrIdStartIdx += 5;
+                        numString = CodeContents.Substring(qrIdStartIdx);
+                    }
+                }
+            }
+            else
+            {
+                //Barcode -- should just contain numbers.
+                numString = this.CodeContents;
+            }
+
+            if (numString != null)
+            {
+                // This code can throw exception.
+                return Int32.Parse(numString);
+            }
+            else
+            {
+                throw new Exception("Could not parse scanned qr code: " + this.CodeContents);
+            }
+        }
+    }
+}
