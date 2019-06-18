f006696f366fd554ccc6965c48a1b321ce0536ab
Dariusz <dariuszjarosz@me.com>
2019-06-18 10:50:58 -0500

    Add a new page that allows user to relocate multiple items.

---
+++ Component.DB/ViewModels/MultiItemRelocateViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Gov.ANL.APS.CDB.Model;
+
+namespace Component.DB.ViewModels
+{
+    public class MultiItemRelocateViewModel : BaseViewModel
+    {
+
+        private Item _SelectedLocation;
+        public ObservableCollection<ItemDetailEditViewModel> LocatableItemList { get; set; }
+
+        public MultiItemRelocateViewModel()
+        {
+            Title = "Relocate Items";
+            LocatableItemList = new ObservableCollection<ItemDetailEditViewModel>(); 
+        }
+
+        public String addItemByQrId(int qrId)
+        {
+            var result = itemApi.GetItemByQrId(qrId);
+
+            if (result.Domain.Name.Equals(Constants.locationDomainName))
+            {
+                this.SelectedLocation = result; 
+                return "Changed location by QrId: " + qrId; 
+            }
+
+
+            // Verify not already in list
+            foreach (var item in LocatableItemList)
+            {
+                var id = item.Item.Id; 
+                if (result.Id == id)
+                {
+                    return "The item with QrId " + qrId + " has already been scanned."; 
+
+                }
+            }
+            
+            // TODO Verify permission
+
+            //Add
+            var editDetailModel = new ItemDetailEditViewModel(result);
+            LocatableItemList.Add(editDetailModel);
+            return "Added item with QrId: " + qrId;
+        }
+
+        public void ClearItems()
+        {
+            LocatableItemList.Clear(); 
+        }
+
+        public Item SelectedLocation
+        {
+            get
+            {
+                return _SelectedLocation; 
+            } set
+            {
+                _SelectedLocation = value;
+                OnPropertyChanged(); 
+            }
+        }
+    }
+}
---
+++ Component.DB/Views/itemEditPages/MultiItemRelocatePage.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using Component.DB.Services;
+using Component.DB.Services.CdbEventArgs;
+using Component.DB.Services.PlatformDependency;
+using Component.DB.ViewModels;
+using Component.DB.Views.PreferencePages;
+using Gov.ANL.APS.CDB.Model;
+using Xamarin.Forms;
+
+namespace Component.DB.Views.itemEditPages
+{
+    public partial class MultiItemRelocatePage : CdbBasePage
+    {
+
+        MultiItemRelocateViewModel viewModel;
+        INotificationPopup NotificationPopup;
+
+        bool loginChecked;
+
+        public MultiItemRelocatePage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new MultiItemRelocateViewModel();
+
+            NotificationPopup = DependencyService.Get<INotificationPopup>();
+            loginChecked = false;
+        }
+
+        public void addQrId(int qrId)
+        {           
+            try
+            {
+                var result = viewModel.addItemByQrId(qrId);
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    NotificationPopup.shortPopup(result);
+                });
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        void HandleClearItemsClicked(object sender, System.EventArgs e)
+        {
+            viewModel.ClearItems();
+        }
+
+        async void HandleScanClicked(object sender, System.EventArgs e)
+        {
+            var scanPage = new QrScannerPage();
+            await Navigation.PushAsync(scanPage);
+        }
+
+        void HandleSelectLocationManually(object sender, System.EventArgs e)
+        {
+            ItemLocationSelectionPage locationSelectionPage = new ItemLocationSelectionPage();
+            locationSelectionPage.LocationSelected += LocationChangedEventHandler;
+
+            Navigation.PushAsync(locationSelectionPage);
+        }
+
+        void LocationChangedEventHandler(Object sender, LocationSelectedEventArgs args)
+        {
+            var location = args.SelectedLocation;
+            viewModel.SelectedLocation = location;
+        }
+
+        async void HandleRelocateItemsClicked(object sender, System.EventArgs e)
+        {
+            foreach (var item in viewModel.LocatableItemList)
+            {
+                try
+                {
+                    var locInfo = item.LoadItemLocationInformation();
+                    ItemDomainLocation locationItem = null;
+                    var selectedLocation = viewModel.SelectedLocation;
+
+                    if (selectedLocation != null)
+                    {
+                        locationItem = new ItemDomainLocation { Id = selectedLocation.Id };
+                    }
+
+                    locInfo.LocationItem = locationItem;
+                    await item.UpdateItemLocationAsync();
+                }
+                catch (Exception ex)
+                {
+                    HandleException(ex);
+                    return;
+                }
+            }
+
+            viewModel.ClearItems();
+            viewModel.SelectedLocation = null;
+            await DisplayAlert("Update Complete", "Locations have been updated", "OK");
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var factory = CdbApiFactory.Instance;
+            var authenticated = await factory.verifyUserAuthenticated();
+            if (authenticated == null || (bool)!authenticated)
+            {
+                var proceedToLogin = await DisplayAlert("Login Needed", "Proceed to login?", "Yes", "No");
+                if (proceedToLogin)
+                {
+                    await Navigation.PushAsync(new LoginPage());
+                } else
+                {
+                    var mainPage = Application.Current.MainPage as MainPage;
+                    await mainPage.NavigateFromMenu((int)MenuItemType.BrowseCatalog); 
+                }
+            }
+        }
+    }
+}
