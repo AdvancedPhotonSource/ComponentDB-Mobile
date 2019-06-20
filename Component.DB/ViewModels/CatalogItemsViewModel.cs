a493a445e005e4f0ae0ac93af01c0a49253ee823
Dariusz <dariuszjarosz@me.com>
2019-06-20 10:44:44 -0500

    Add ability to fetch catalog items by favorites or all. Send messages to view when trouble loading favorites exists.

--- Component.DB/ViewModels/CatalogItemsViewModel.cs
+++ Component.DB/ViewModels/CatalogItemsViewModel.cs
@@ -12,6 +12,7 @@ using Xamarin.Forms;
 using Component.DB.Views;
 using System.Collections.Generic;
 using Gov.ANL.APS.CDB.Model;
+using Gov.ANL.APS.CDB.Client;
 
 namespace Component.DB.ViewModels
 {
@@ -23,7 +24,43 @@ namespace Component.DB.ViewModels
 
         public override async Task<List<Item>> getItems()
         {
-            List<ItemDomainCatalog> itemCatalogList = await itemApi.GetCatalogItemsAsync();
+            List<ItemDomainCatalog> itemCatalogList = null;
+
+            var result = AppStorage.GetCatalogBrowseMode();
+            if (result == Services.CdbMobileAppStorage.BrowseMode.Favorites)
+            {
+
+                Title = getTitle() + " (Favorites)";
+                try
+                {
+                    itemCatalogList = await itemApi.GetFavoriteCatalogItemsAsync();
+
+                    if (itemCatalogList == null)
+                    {
+                        FireViewModelMessageEvent("It seems that current user does not have favorites. Loading full list.");
+                    }
+                } catch (ApiException ex)
+                {
+                    // Unauthorized Exception
+                    if (ex.ErrorCode == 401)
+                    {
+                        // Ignore unauthroized exception but notify user. 
+                        // To do, transfer message to view via event. 
+                        FireViewModelMessageEvent("Cannot Load Favorites. Please log in and try again.");
+                    } else
+                    {
+                        throw ex;
+                    }
+                    Debug.WriteLine(ex);
+                }
+            }
+
+            if (itemCatalogList == null)
+            {
+                Title = getTitle();
+                itemCatalogList = await itemApi.GetCatalogItemsAsync(); 
+            }
+
             List<Item> itemList = itemCatalogList.ConvertAll(x => (Item)x); 
 
             return itemList; 
