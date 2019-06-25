941b18ab1ead3c0a2eae2388710c07b6a7a6f23d
Dariusz <dariuszjarosz@me.com>
2019-06-25 09:39:42 -0500

    Add view that enables the user to add log entries to an item.

---
+++ Component.DB/ViewModels/AddLogEntryViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Gov.ANL.APS.CDB.Model;
+
+namespace Component.DB.ViewModels
+{
+    public class AddLogEntryViewModel : BaseViewModel
+    {
+        public String LogEntry { get; set; }
+        public DateTime EffectiveDate { get; set; }
+
+        private int ItemId { get; }
+
+        public AddLogEntryViewModel(int ItemId)
+        {
+            Title = "Add Log Entry";
+            this.ItemId = ItemId; 
+
+            LogEntry = "";
+            EffectiveDate = DateTime.Today;
+        }
+
+        public async Task AddLogEntryForItemAsync()
+        {
+            var logEntryItem = new LogEntryEditInformation
+            {
+                ItemId = ItemId,
+                EffectiveDate = EffectiveDate,
+                LogEntry = LogEntry
+            };
+
+            await itemApi.AddLogEntryToItemAsync(logEntryItem); 
+        }
+    }
+}
---
+++ Component.DB/Views/itemEditPages/AddLogEntryPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Component.DB.ViewModels;
+
+namespace Component.DB.Views.itemEditPages
+{
+    public partial class AddLogEntryPage : CdbBasePage
+    {
+        AddLogEntryViewModel viewModel;
+
+        public AddLogEntryPage(int ItemId)
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new AddLogEntryViewModel(ItemId); 
+        }
+
+        async void HandleSaveClickedAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                await viewModel.AddLogEntryForItemAsync();
+                await Navigation.PopAsync();
+            } catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+    }
+}
